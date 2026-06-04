e7306f4f636f9de765d02c46b55058db862f8c41
alegp2020 <alexpicos644@gmail.com>
2026-06-04 16:02:19 +0200

    ultimos retoques

--- nextjs-dashboard/app/gestor/page.tsx
+++ nextjs-dashboard/app/gestor/page.tsx
@@ -3,12 +3,19 @@ import { useState } from 'react';
 export default function Page() {
     const [titulo, settitulo] = useState("");
     const [descripcion, setdescripcion] = useState("");
+    const [contador, setContador] = useState(0);
+
     // guaradra la tearea
 
     const [tareaG, setTareaG] = useState<Tarea[]>([]);
 
+    // lista de tareas completadas
+    const [tareaComplet, setTareaComplet] = useState<Tarea[]>([]);
+    const [mostrarTareasCompletadas, setMostrarTareasCompletadas] = useState(false);
+
     // para ponerlo caundo cree una nueva tarea
     type Tarea = {
+        Id: number;
         Titulo: string;
         Descripcion: string;
     }
@@ -17,8 +24,9 @@ export default function Page() {
         if (titulo.trim() && descripcion.trim()) {
             //crea la nueva tarea con todo eso utioliando lo quepuse al principo del type
             const nuevaTarea: Tarea = {
+                Id: Date.now(),
                 Titulo: titulo,
-                Descripcion: descripcion,
+                Descripcion: descripcion
             };
 
             //actualiza la atera gauradndfo todo lo qu ehemos anteiromente 
@@ -31,8 +39,30 @@ export default function Page() {
         }
     }
 
+    // Para eliminar filtramos toda la lista de tarea exceptuando la que queremos eliminar
+    function eliminar(id: number){
+        // Me quedo con todas las tareas menos esa
+        setTareaG(tareaG.filter(tarea => tarea.Id != id));
+    }
 
+    // Al marcar una tarea como completada se recupera de la lista de tareas y se introduce en la lista de completadas
+    // Luego la eliminamos
+    // Incrementamos el contador
+    function completada(id:number){
+        let tarea = tareaG.filter(tarea => tarea.Id == id);
+        setTareaComplet([...tareaComplet, tarea[0]])
+        eliminar(id);
+        setContador(contador+1);
+    }
 
+    function verTareas(){
+        if (mostrarTareasCompletadas){
+            setMostrarTareasCompletadas(false);
+
+        }else{
+            setMostrarTareasCompletadas(true);
+        }
+    }
 
     return (
         <>
@@ -69,20 +99,48 @@ export default function Page() {
                         Lista de tareas
                     </h2>
 
+                    <h4>Tareas completadas: {contador}</h4>
+
                     <ul className="flex flex-col gap-2">
                         {tareaG.map((tarea, i) => (
+                            
                             <li
                                 key={i}
                                 className="border border-sky-200 rounded-xl p-5 bg-sky-50 w-64 shadow-sm"
                             >
+                                <button onClick = {() => eliminar(tarea.Id)} className="bg-red-200 p-2 m-1 rounded">X</button>
+                                <button onClick = {() => completada(tarea.Id)} className="bg-blue-200 p-2 m-1 rounded">O</button>
                                 {tarea.Titulo} - {tarea.Descripcion}
+                            
                             </li>
                         ))}
                     </ul>
                 </div>
 
+                
+                
 
             </main >
+
+            {/* MOSTRAR TAREAS COMPLETADAS */}
+            <div className="flex flex-col mr-60 ml-60 justify-center p-10">
+                        <button className="bg-red-200 p-2 rounded" onClick = {verTareas}>Ver tareas completadas</button>
+                        <br></br>
+{/* Solo si mostrarTareasCompletadas está a true, se mostrará la lista de tareas, sino permanecerá oculta */}
+                {mostrarTareasCompletadas && (<ul className="flex flex-col gap-2">
+                        {tareaComplet.map((tarea, i) => (
+                            
+                            <li
+                                key={i}
+                                className="border border-sky-200 rounded-xl p-5 bg-sky-50 w-64 shadow-sm justify-center"
+                            >
+                                {tarea.Titulo} - {tarea.Descripcion}
+                            
+                            </li>
+                        ))}
+                    </ul>)}
+
+                </div>
         </>
     );
 } 
